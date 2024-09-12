ece8ce0f513fd6b0691d9a88ff10ccb3353d2d90
adamn1225 <anoah1225@gmail.com>
2024-09-12 16:36:38 -0400

    something small..

--- src/app/tools/page.tsx
+++ src/app/tools/page.tsx
@@ -6,20 +6,20 @@ import MetersToFeetInchesConverter from '@/components/MetersToFeetInchesConverte
 import MmToFeetInchesConverter from '@/components/MmToFeetInchesConverter';
 import Aside from '@/components/Aside';
 
-export function Calculators() {
+const Calculators: React.FC = () => {
     return (
-        <>
-            <div className="flex flex-col gap-2 h-screen justify-items-center items-start w-full pl-[53px]">
-                <Aside />
-        <div className='flex w-screen h-1/3 justify-center items-center gap-12'>
+        <div className="flex flex-col gap-2 h-screen justify-items-center items-start w-full pl-[53px]">
+            <Aside />
+            <div className='flex w-screen h-1/3 justify-center items-center gap-12'>
                 <KmToMiles />
                 <TonsToLbs />
                 <MetersToFeetInchesConverter />
                 <MmToFeetInchesConverter />
-           </div>
-            <div className="flex items-center justify-center w-full pt-24 mt-6 border-t-2 border-slate-950">  <ZipCodeLookup /></div>
             </div>
-        </>
+            <div className="flex items-center justify-center w-full pt-24 mt-6 border-t-2 border-slate-950">
+                <ZipCodeLookup />
+            </div>
+        </div>
     );
 }
 
