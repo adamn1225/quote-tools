f4fd8a2641cfb5535ff3459c4620a65fa555e4b2
adamn1225 <anoah1225@gmail.com>
2024-09-12 16:18:51 -0400

    page error

--- src/app/equipment-directory/page.tsx
+++ src/app/equipment-directory/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from 'react'
 import DimensionSearch from "@/components/DimensionSearch";
 import Aside from '@/components/Aside';
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from 'react';
 import Aside from '@/components/Aside';
 import DimensionSearch from '@/components/DimensionSearch';
