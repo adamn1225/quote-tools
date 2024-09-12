d007ccb973cad0fcfcb28b95f04a0a575d13f563
adamn1225 <anoah1225@gmail.com>
2024-09-12 17:04:41 -0400

    something small..

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -13,12 +13,12 @@ import { Bolt, Settings, Bot, FolderSearch2, Calculator, LucideTruck } from "luc
 function Aside() {
     return (
         <TooltipProvider>
-            <div className="pl-[200px] bg-gray-950 z-50">
+            <div className="pl-[200px] bg-gray-950 z-50 overflow-hidden shadow-lg shadow-slate-500">
                 <aside className="inset-y fixed left-0 z-50 bg-gray-950 flex h-full flex-col border-r w-[260px]">
-                    <div className="border-b p-2 w-full flex justify-center items-center ">
-                        <Button aria-label="Home">
+                    <div className="border-b p-2 w-full max-w-min overflow-hidden flex justify-center items-center ">
+                        <Button className='border bg-gray-950 rounded-sm border-white p-8' aria-label="Home">
                             <Bolt className="size-8 bg-white fill-foreground" />
-                            <h2 className='font-bold pl-4 text-xl font-mono'>HEAVY TOOLS</h2>
+                            <h2 className='font-bold pl-4 md:text-lg lg:text-xl font-mono'>HEAVY TOOLS</h2>
                         </Button>
                     </div>
                     <nav className="grid gap-1 justify-center pt-8">
@@ -31,11 +31,11 @@ function Aside() {
                                         aria-label="Equipment Directory"
                                     >
                                         <FolderSearch2 className="size-5" />
-                                        <span>Equipment Directory</span>
+                                        <span>EQUIPMENT DIRECTORY</span>
                                     </Button>
                                 </Link>
                             </TooltipTrigger>
-                            <TooltipContent side="right" sideOffset={5}>
+                            <TooltipContent side="right" sideOffset={7}>
                                 Equipment Directory
                             </TooltipContent>
                         </Tooltip>
@@ -45,15 +45,15 @@ function Aside() {
                                 <Link href="/tools">
                                     <Button
                                         variant="ghost"
-                                        className="rounded-lg flex justify-start items-start w-full gap-2 bg-white"
+                                        className="rounded-lg flex justify-start items-center w-full gap-2 bg-white"
                                         aria-label="Tools"
                                     >
                                         <Calculator className="size-5" />
-                                        <span>Tools</span>
+                                        <span>TOOLS</span>
                                     </Button>
                                 </Link>
                             </TooltipTrigger>
-                            <TooltipContent side="right" sideOffset={5}>
+                            <TooltipContent side="right" sideOffset={7}>
                                 Tools
                             </TooltipContent>
                         </Tooltip>
@@ -63,15 +63,15 @@ function Aside() {
                                 <Link href="/settings">
                                     <Button
                                         variant="ghost"
-                                        className="rounded-lg flex justify-start w-full items-center gap-2 bg-white"
+                                        className="rounded-lg h-12 flex justify-start w-full items-center bg-white gap-1"
                                         aria-label="Settings"
                                     >
                                         <Settings className="size-5" />
-                                        <span>Settings</span>
+                                        <span className='text-xs text-justify'>SETTINGS<br />(coming soon)</span>
                                     </Button>
                                 </Link>
                             </TooltipTrigger>
-                            <TooltipContent side="right" sideOffset={5}>
+                            <TooltipContent side="right" sideOffset={7}>
                                 Settings
                             </TooltipContent>
                         </Tooltip>
@@ -81,15 +81,15 @@ function Aside() {
                                 <Link href="/help">
                                     <Button
                                         variant="ghost"
-                                        className="rounded-lg bg-white justify-start w-full flex items-center gap-2"
-                                        aria-label="Help"
+                                        className="rounded-lg h-12 bg-white justify-start w-full flex items-center gap-1"
+                                        aria-label="Assistance"
                                     >
                                         <Bot className="size-5" />
-                                        <span>Help</span>
+                                        <span className='text-xs text-justify'>BROKER ASSISTANT<br />(coming soon)</span>
                                     </Button>
                                 </Link>
                             </TooltipTrigger>
-                            <TooltipContent side="right" sideOffset={5}>
+                            <TooltipContent side="right" sideOffset={7}>
                                 Help
                             </TooltipContent>
                         </Tooltip>
